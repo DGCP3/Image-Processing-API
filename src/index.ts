2d758857d682208f83768608dffe402ca69ea354
dgcp3 <zdagmawig1@gmail.com>
2022-06-30 00:14:34 +0300

    feat(server): setup server

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
 import express, { Application } from 'express'
-
-import exampleRouter from './routes/example'
-
+import imageRouter from './routes/imageRoute'
 const app: Application = express()
 
 /* ROUTES */
-app.use('/example', exampleRouter)
-
+app.use('/images', imageRouter)
 /* LISTENING */
-const PORT: Number = 4000
+const PORT = 4000
 app.listen(PORT, (): void => console.log(`running on port ${PORT}`))
 
+process.on('unhandledRejection', (err) => {
+	console.error('Error:\n', err)
+	process.exit(1)
+})
 export default app
