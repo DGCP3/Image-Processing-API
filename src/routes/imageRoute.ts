bf92380b1ec40b3d8e5730822e607da0e3af87e0
dgcp3 <zdagmawig1@gmail.com>
2022-06-30 12:01:20 +0300

    feat(Router): add no query middleware

--- src/routes/imageRoute.ts
+++ src/routes/imageRoute.ts
@@ -1,17 +1,18 @@
 import { Router } from 'express'
 import { serveImg } from '../controllers/imageController'
-import { fileExist, getImgMetaData, checkProcessedImg, processImg, makeDir } from '../middleware/image_middleware'
+import { fileExist, getImgMetaData, checkProcessedImg, processImg, makeDir, noQuery } from '../middleware'
 
 const router = Router()
 const imageTypes = ['.jpg', '.jpeg', '.png', '.gif', '.webp']
 router.get(
 	'/',
 	fileExist('./src/assets/images/', imageTypes),
+	noQuery,
 	makeDir,
 	getImgMetaData('./src/assets/thumb/'),
 	checkProcessedImg,
 	processImg,
-	serveImg,
+	serveImg('./src/assets/thumb/'),
 )
 
 export default router
