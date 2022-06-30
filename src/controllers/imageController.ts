7d019ba47f8fd20004d98b73968f494f101122ba
dgcp3 <zdagmawig1@gmail.com>
2022-06-30 11:59:00 +0300

    feat(Controllers): controllers

---
+++ src/controllers/imageController.ts
@@ -0,0 +1,12 @@
+import { Response } from 'express'
+import ImageHandler from '../services/image'
+import { RequestWithMetadata } from '../types/express'
+
+export const serveImg = (path: string) => async (req: RequestWithMetadata, res: Response) => {
+	const { filename, width, height, format } = req.query
+	const { imgBuffer } = req
+	res.writeHead(200, { 'Content-Type': `image/${format ?? 'jpeg'} ` })
+	res.end(imgBuffer)
+	await ImageHandler.toFile(imgBuffer as Buffer, `${path}${width}x${height}/${filename}`)
+	// res.sendFile(path.join(__dirname, `../assets/thumb/${width}x${height}/${filename}`))
+}
