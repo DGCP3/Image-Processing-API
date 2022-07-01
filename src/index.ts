4553317b1f8d9b7d69be404cf0a8f46a961b23c8
dgcp3 <zdagmawig1@gmail.com>
2022-07-01 17:28:24 +0300

    remove export

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express, { Application } from 'express'
 import imageRouter from './routes/imageRoute'
+
 const app: Application = express()
 
 /* ROUTES */
 app.use('/images', imageRouter)
+
 /* LISTENING */
 const PORT = 4000
 app.listen(PORT, (): void => console.log(`running on port ${PORT}`))
@@ -12,4 +14,3 @@ process.on('unhandledRejection', (err) => {
 	console.error('Error:\n', err)
 	process.exit(1)
 })
-export default app
