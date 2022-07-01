94c4eea4698a7acea94b9912b25f3b1e6ce554b6
dgcp3 <zdagmawig1@gmail.com>
2022-07-02 00:59:54 +0300

    fix: :label: add type for functions

--- src/services/image.ts
+++ src/services/image.ts
@@ -1,30 +1,30 @@
 import sharp, { FormatEnum } from 'sharp'
 
 export default class ImageHandler {
-	static async resize(file: Buffer, width: number, height: number) {
+	static async resize(file: Buffer, width: number, height: number): Promise<Buffer> {
 		return await sharp(file)
 			.resize(width as number, height as number)
 			.toBuffer()
 	}
-	static async rotate(file: Buffer, angle: number | undefined) {
+	static async rotate(file: Buffer, angle: number | undefined): Promise<Buffer> {
 		return await sharp(file).rotate(angle).toBuffer()
 	}
-	static async flip(file: Buffer, direction: boolean | undefined) {
+	static async flip(file: Buffer, direction: boolean | undefined): Promise<Buffer> {
 		return await sharp(file).flip(direction).toBuffer()
 	}
-	static async toFile(buffer: Buffer, filePath: string) {
+	static async toFile(buffer: Buffer, filePath: string): Promise<sharp.OutputInfo> {
 		return await sharp(buffer).toFile(filePath)
 	}
-	static async blur(file: Buffer, sigma?: number) {
+	static async blur(file: Buffer, sigma?: number): Promise<Buffer> {
 		return await sharp(file).blur(sigma).toBuffer()
 	}
-	static async sharpen(file: Buffer, sigma?: number) {
+	static async sharpen(file: Buffer, sigma?: number): Promise<Buffer> {
 		return await sharp(file).sharpen(sigma).toBuffer()
 	}
-	static async metadata(file: string) {
+	static async metadata(file: string): Promise<sharp.Metadata> {
 		return await sharp(file).metadata()
 	}
-	static async toFormat<T extends keyof FormatEnum>(file: Buffer, format: T = 'jpg' as T) {
+	static async toFormat<T extends keyof FormatEnum>(file: Buffer, format: T = 'jpg' as T): Promise<Buffer> {
 		return await sharp(file).toFormat(format).toBuffer()
 	}
 }
