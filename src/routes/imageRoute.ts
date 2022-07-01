df8a24121c87ba54ec5c446599e3aa79eb84ac6c
dgcp3 <zdagmawig1@gmail.com>
2022-07-01 17:31:33 +0300

    feat(Middleware): add query checker midd

--- src/routes/imageRoute.ts
+++ src/routes/imageRoute.ts
@@ -1,13 +1,13 @@
 import { Router } from 'express'
 import { serveImg } from '../controllers/imageController'
-import { fileExist, getImgMetaData, checkProcessedImg, processImg, makeDir, noQuery } from '../middleware'
+import { fileExist, getImgMetaData, checkProcessedImg, processImg, makeDir, checkQuery } from '../middleware'
 
 const router = Router()
 const imageTypes = ['.jpg', '.jpeg', '.png', '.gif', '.webp']
 router.get(
 	'/',
 	fileExist('./src/assets/images/', imageTypes),
-	noQuery,
+	checkQuery(imageTypes),
 	makeDir,
 	getImgMetaData('./src/assets/thumb/'),
 	checkProcessedImg,
