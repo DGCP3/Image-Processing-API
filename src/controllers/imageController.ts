420139cdc32ba0bfac1486ed1ec760de85a7a2f6
dgcp3 <zdagmawig1@gmail.com>
2022-07-01 17:31:09 +0300

    refactor(Controllers): refac

--- src/controllers/imageController.ts
+++ src/controllers/imageController.ts
@@ -5,8 +5,10 @@ import { RequestWithMetadata } from '../types/express'
 export const serveImg = (path: string) => async (req: RequestWithMetadata, res: Response) => {
 	const { filename, width, height, format } = req.query
 	const { imgBuffer } = req
-	res.writeHead(200, { 'Content-Type': `image/${format ?? 'jpeg'} ` })
+	res.writeHead(200, { 'Content-Type': `image/${format ?? 'jpeg'}` })
 	res.end(imgBuffer)
-	await ImageHandler.toFile(imgBuffer as Buffer, `${path}${width}x${height}/${filename}`)
-	// res.sendFile(path.join(__dirname, `../assets/thumb/${width}x${height}/${filename}`))
+	await ImageHandler.toFile(
+		imgBuffer as Buffer,
+		`${path}${width}x${height}/${filename?.toString().split('.')[0]}.${format ?? 'jpg'}`,
+	)
 }
