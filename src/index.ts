fcc9187eec4fc897609051e6c6688e8c9acd9cd3
dgcp3 <zdagmawig1@gmail.com>
2022-07-08 14:38:17 +0300

    refactor: add health route

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,14 @@ import imageRouter from './routes/imageRoute'
 const app: Application = express()
 
 /* ROUTES */
+app.get('/', (req, res) => {
+	res.status(200).send('Health Check')
+})
 app.use('/images', imageRouter)
 
 /* LISTENING */
 const PORT = 4000
 app.listen(PORT, (): void => console.log(`running on port ${PORT}`))
-
 process.on('unhandledRejection', (err) => {
 	console.error('Error:\n', err)
 	process.exit(1)
